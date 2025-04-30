2bc953138aba4bffb5b4d4ddb6f96880f04aec55
0k1na <rin17tennis@gmail.com>
2025-04-30 22:27:05 +0900

    UIの仮追加

---
+++ UnityProject/Assets/Scripts/HPBar.cs
@@ -0,0 +1,30 @@
+/*=====
+<HPBar.cs>	// スクリプト名
+└作成者：okugami
+
+＞HPの変動をアニメーションするためのプログラム
+
+＞更新履歴
+__Y25	
+_M04	
+D		
+23:プログラム作成:okugami	
+=====*/
+
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
